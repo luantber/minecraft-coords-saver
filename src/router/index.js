570a29d3eec56e307fcffaed0efa601c5affc5ce
Luis Bernal <luisbch9@gmail.com>
2020-09-21 11:46:00 -0500

    no comments

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,58 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Land from '../views/Land.vue'
-import {firebase } from "../firebase"
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Land from "../views/Land.vue"
+import { firebase } from "../firebase"
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Land',
-    component: Land,
-    beforeEnter: (to, from, next) => {
-      if ( firebase.auth().currentUser  ) next({
-        path:"/worlds"
-      })
-      else
-        next()
-    }
-  },
+const routes = [
+	{
+		path: "/",
+		name: "Land",
+		component: Land,
+		beforeEnter: (to, from, next) => {
+			if (firebase.auth().currentUser)
+				next({
+					path: "/worlds"
+				})
+			else next()
+		}
+	},
 
-  {
-    path: '/worlds',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/worlds/:id',
-    name: 'World',
-    component: () => import('../views/World.vue'),
-    meta: { requiresAuth: true }
-  }
+	{
+		path: "/worlds",
+		name: "Home",
+		component: () => import("../views/Home.vue"),
+		meta: { requiresAuth: true }
+	},
+	{
+		path: "/worlds/:id",
+		name: "World",
+		component: () => import("../views/World.vue"),
+		meta: { requiresAuth: true }
+	}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes
 })
-
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if ( ! firebase.auth().currentUser  ) {
-      next({
-        path: '/',
-        query: { redirect: to.fullPath }
-      })
-    } else {
-      next()
-    }
-  } else {
-    next() // make sure to always call next()!
-  }
+	if (to.matched.some((record) => record.meta.requiresAuth)) {
+		// this route requires auth, check if logged in
+		// if not, redirect to login page.
+		if (!firebase.auth().currentUser) {
+			next({
+				path: "/",
+				query: { redirect: to.fullPath }
+			})
+		} else {
+			next()
+		}
+	} else {
+		next() // make sure to always call next()!
+	}
 })
-
 
 export default router
